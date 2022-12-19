ed9345255ee33058c793b6463222f84f310cb098
hssh0413 <bockman822@gmail.com>
2022-12-19 22:12:57 +0900

    test.py

---
+++ DAY1/test.py
@@ -0,0 +1,24 @@
+'''
+출력 print
++ 더하기
+- 뺴기
+* 곱하기
+/ 나누기
+** 거듭제곱
+// 몫
+% 나머지
+'''
+print("python 시작")
+apple=10
+orange=20
+print(orange+apple)
+print(orange-apple)
+print(orange*apple)
+print(orange/apple)
+print(1/3)
+print(2**2)
+print(3**3)
+print(orange//apple)
+print(10//3)
+print(10%3)
+print(orange%apple)
