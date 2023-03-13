51738a29e544ac8b22d0070588c495bc2c201ce8
hssh0413 <bockman822@gmail.com>
2023-03-13 22:28:00 +0900

    1차,2차함수 그래프

---
+++ DAY6/graph.py
@@ -0,0 +1,40 @@
+'''
+module 설치
+-터미널 창에서 pip 설치 명령어 사용:pip install module이름
+-설치된 module을 확인:pip list
+-module 불러오기:import module이름
+-module 사용하기:module이름.method(),module이름.변수
+'''
+import matplotlib.pyplot as plt
+# plt.figure()
+# x1=[0,100]
+# y1=[0,100]
+# x2=[0,100]
+# y2=[0,50]
+# plt.plot(x1,y2,'r',x2,y2,'g')
+# plt.xlabel("x axis")
+# plt.ylabel("y axis")
+# plt.title("y=x+1,y=1/2x")
+# plt.show()
+
+# x=[]
+# y=[]
+# for i in range(100):
+#     x.append(i)
+#     y.append(-3*i+5)
+# plt.figure()
+# plt.plot(x,y)
+# plt.xlabel("x axis")
+# plt.ylabel("y axis")
+# plt.show()
+
+# x=[]
+# y=[]
+# for i in range(-100,100):
+#     x.append(i)
+#     y.append(i*i)
+# plt.figure()
+# plt.plot(x,y)
+# plt.xlabel("x axis")
+# plt.ylabel("y axis")
+# plt.show()
